d618beb77be79c4e284c7a5a8d10797e760f9bbc
Abdulshakur11 <abdulshakurturdimatov@gmail.com>
2021-09-22 19:25:31 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -4,18 +4,19 @@ var elCelsiusInput = document.querySelector('.celsius-input');
 var elFahrenheitinput = document.querySelector('.fahrenheit-input');
 
 // RESULRT ELEMENTS FROM HTML
-var elCelsiusResult = document.querySelector('.result-celsius');
+var elCelciusResult = document.querySelector('.result-celsius');
 
- // FUNCTION
- elFahrenheitinput.addEventListener('input', function(){
-  var elFahrenheitNumber = (parseFloat(elFahrenheitinput.value.trim(), 10)- 32 ) * 5/9;
-  elCelsiusInput.value = elFahrenheitNumber;
-  elCelsiusResult.textContent = '(' + elFahrenheitinput.value + '°F-32)×5/9'
+// FUNCTION
+ elCelsiusInput.addEventListener('input', function(){
+  var elFahrenheitResult = parseFloat(elCelsiusInput.value.trim(), 10) * 9/5 + 32;
+  elFahrenheitinput.value = elFahrenheitResult;
+  elCelciusResult.textContent = elCelsiusInput.value + '°C×9/5+32 =' + elFahrenheitResult;
  });
 
 
- elCelsiusInput.addEventListener('input', function(){
-  var elCelsiusNumber = parseFloat(elCelsiusInput.value.trim(), 10) * 9/5 + 32;
-  elFahrenheitinput.value = elCelsiusNumber;
-  elCelsiusResult.textContent = elCelsiusInput.value + '°C×9/5+32'
+ // FUNCTION
+ elFahrenheitinput.addEventListener('input', function(){
+  var elCelsiusResult = (parseFloat(elFahrenheitinput.value.trim(), 10)- 32 ) * 5/9;
+  elCelsiusInput.value = elCelsiusResult;
+  elCelciusResult.textContent = '(' + elFahrenheitinput.value + '°F-32)×5/9 =' + elCelsiusResult;
  });
